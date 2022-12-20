325204295bcb82970e61971bf986e723ec9cf9fc
Pablo <pmoleri@infragistics.com>
2022-12-20 19:55:11 -0300

    Setup MAUI Blazor hybrid app

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -1,55 +1,29 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using IgniteUI.Blazor.Controls;
-using MovieApp;
-
+using Microsoft.Extensions.DependencyInjection;
 using MovieApp.MovieAppData;
+using IgniteUI.Blazor.Controls;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
-
-
-builder.Services.AddScoped<MovieAppDataService>();
-RegisterIgniteUI(builder.Services);
-
-void RegisterIgniteUI(IServiceCollection services)
+public class MovieAppShared
 {
-    services.AddIgniteUIBlazor(
-        typeof(IgbNavDrawerModule),
-        typeof(IgbRippleModule),
-        typeof(IgbIconButtonModule),
-        typeof(IgbAvatarModule),
-        typeof(IgbDropdownModule),
-        typeof(IgbDropdownItemModule),
-        typeof(IgbButtonModule),
-        typeof(IgbTabsModule),
-        typeof(IgbCardModule),
-        typeof(IgbDatePickerModule),
-        typeof(IgbListModule)
-    );
+    public static void Register(IServiceCollection services)
+    {
+        services.AddScoped<MovieAppDataService>();
+        RegisterIgniteUI(services);
+    }
+
+    public static void RegisterIgniteUI(IServiceCollection services)
+    {
+        services.AddIgniteUIBlazor(
+            typeof(IgbNavDrawerModule),
+            typeof(IgbRippleModule),
+            typeof(IgbIconButtonModule),
+            typeof(IgbAvatarModule),
+            typeof(IgbDropdownModule),
+            typeof(IgbDropdownItemModule),
+            typeof(IgbButtonModule),
+            typeof(IgbTabsModule),
+            typeof(IgbCardModule),
+            typeof(IgbDatePickerModule),
+            typeof(IgbListModule)
+        );
+    }
 }
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
---
+++ MovieAppWeb/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MovieApp;
+
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseStaticWebAssets();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+
+MovieAppShared.Register(builder.Services);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
